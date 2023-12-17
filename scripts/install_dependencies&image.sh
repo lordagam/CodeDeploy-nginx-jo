af45009198b83e37befe0acaf8c4ef9b9eb38613
joseph agam <127337524+lordagam@users.noreply.github.com>
2023-12-17 14:42:14 +0200

    Update install_dependencies&image.sh

--- scripts/install_dependencies&image.sh
+++ scripts/install_dependencies&image.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-sudo yum update -y
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-sudo yum install docker-engine
-sudo systemctl enable docker.service
-sudo systemctl start docker
+set -e
+
+docker load -i /tmp/alpine-minirootfs-3.17.6-x86_64.tar.gz
