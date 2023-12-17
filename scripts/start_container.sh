91ec7cde9c177fc4c31de772ee56c66077a93bb5
joseph agam <127337524+lordagam@users.noreply.github.com>
2023-12-17 16:48:28 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo systemctl start docker
-sudo systemctl enable docker
+#sudo systemctl start docker
+#sudo systemctl enable docker
 docker run -d --name nginx-jo -p 8000:80 nginx-jo
