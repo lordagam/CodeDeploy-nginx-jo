a1903c3f6c704ff15594970ce09cc6e6e18222ba
joseph agam <127337524+lordagam@users.noreply.github.com>
2023-12-17 19:09:10 +0200

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
+docker stop nginx-jo
 #docker rm nginx-jo
 
+
 # Name of the container you want to stop
-container_name="nginx-jo"
+#container_name="nginx-jo"
 
 # Check if the container exists and is running
-is_container_running=$(docker inspect --format="{{.State.Running}}" $container_name 2>/dev/null)
+#is_container_running=$(docker inspect --format="{{.State.Running}}" $container_name 2>/dev/null)
 
-if [[ "$is_container_running" == "true" ]]; then
+#if [[ "$is_container_running" == "true" ]]; then
     # Stop the container
-    docker stop $container_name
-    echo "Container $container_name stopped successfully."
-else
-    echo "Container $container_name is not running."
-fi
+ #   docker stop $container_name
+  #  echo "Container $container_name stopped successfully."
+#else
+ #   echo "Container $container_name is not running."
+#fi
