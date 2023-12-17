b0aaa0b215e811a6b2c80ba900d8a8cd46d688e9
joseph agam <127337524+lordagam@users.noreply.github.com>
2023-12-17 14:13:26 +0200

    Delete CodeDeploy-nginx-jo directory

--- CodeDeploy-nginx-jo/scripts/install_dependencies&image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo yum check-update
-curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl status docker
-sudo systemctl enable docker
--- CodeDeploy-nginx-jo/scripts/start_container.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker run -d --name nginx-jo -p 8000:80 nginx-jo
