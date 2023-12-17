aaf0cff8833fa9ad96f57a263381c63209c2ae03
joseph agam <127337524+lordagam@users.noreply.github.com>
2023-12-17 14:27:09 +0200

    Update install_dependencies&image.sh
    fix depen 1

--- scripts/install_dependencies&image.sh
+++ scripts/install_dependencies&image.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-sudo yum check-update -y
-curl -fsSL https://get.docker.com/ | sh
+sudo yum update
+$ sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+sudo yum install docker-engine
+sudo systemctl enable docker.service
 sudo systemctl start docker
-sudo systemctl status docker
-sudo systemctl enable docker
